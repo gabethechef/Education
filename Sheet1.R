7da64cbc4c704342a2c39447e4ca0273ee7798b6
gabriel <lujx2002@gmail.com>
2023-01-31 20:54:47 +0800

    Continutation of Case Study

--- Sheet1.R
+++ Sheet1.R
@@ -1,6 +1,8 @@
 Book1 %>% 
-  filter(town == "ANG MO KIO") %>% 
+  filter(town == 'JURONG EAST', flat_type == "4 ROOM") %>% 
   ggplot(aes(x = remaining_lease, y = resale_price)) +
   geom_point() +
-  facet_wrap(facets = vars(flat_type)) + 
-  labs(title = "Resale prices plotted against remaining lease tenure", subtitle = "For HDBs in Ang Mo Kio")
+  geom_smooth(method= lm, se = FALSE)+
+  labs(title = "Relationship between resale prices and lease tenure for 4 Room HDBs", subtitle = 'Estate: Jurong East')
+
+
